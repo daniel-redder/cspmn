d92b8f5a66ab3468dbd8e5f667a85fc648b0b1fb
Daniel Redder <raptersong@gmail.com>
2022-01-21 10:47:30 -0500

    changed random.randint to random.uniform to account for continous bounds of dirchlet

--- cspmn.py
+++ cspmn.py
@@ -52,7 +52,7 @@ class contaminator():
     def e_contam(self, node,rangeW):
         # contaminate them
         print(node.weights)
-        node.weights = self.rng.dirichlet(alpha=[random.randint(rangeW[0],rangeW[1]) for x in node.weights])
+        node.weights = self.rng.dirichlet(alpha=[random.uniform(rangeW[0],rangeW[1]) for x in node.weights])
         return node.weights
 cont = contaminator()
 
